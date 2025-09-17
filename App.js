7b4aafc4fa77537adcf9e66f46d4bad98a27a09f
9 spades <9-spades@proton.me>
2025-09-17 16:15:51 -0600

    Amplify deployment

--- App.js
+++ App.js
@@ -1,11 +1,48 @@
+import { useState } from 'react';
+import { StyleSheet, View, Pressable } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { CardDeck } from '@/components/CardDeck';
 
 export default function App() {
+  const [isFlipped, setIsFlipped] = useState(false);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Pressable
+        onPress={() => {
+          if (!isFlipped) setIsFlipped(!isFlipped);
+        }}
+      >
+        <CardDeck
+          playingCard={{
+            heading: 'You just wait, sunshine',
+            label: '',
+            sublabel: 'Site under development',
+            content: {
+              name: 'Andres Romero',
+              title: 'Software Engineer',
+              email: '9-spades@proton.me',
+              skills: [
+                'Scrum',
+                'API design & development',
+                'Java',
+                'Gradle',
+                'Python',
+                'C',
+                'AWS',
+                'OCI',
+                'Relational DBs',
+                'Docker',
+                'git & ade'
+              ],
+              active: true,
+              experience: 2
+            }
+          }}
+          shouldFlip={isFlipped}
+        />
+      </Pressable>
+      <StatusBar style='auto' />
     </View>
   );
 }
@@ -15,6 +52,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
+    justifyContent: 'center'
+  }
 });
